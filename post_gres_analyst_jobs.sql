c6d92b174307749d8e5b83e06923461c4b08d61a
lo0017 <lauren.odonnell99@gmail.com>
2026-01-28 14:36:55 -0500

    intro_complete

--- post_gres_analyst_jobs.sql
+++ post_gres_analyst_jobs.sql
@@ -4,22 +4,20 @@ FROM ecd;
 SELECT COUNT(DISTINCT company) AS total_companies
 FROM ecd;
 
-SELECT COUNT(DISTINCT company)
+SELECT COUNT(DISTINCT company) as no_grant_count
 FROM ecd
-WHERE company NOT IN (
-    SELECT company 
-    FROM ecd 
-    WHERE ed > '$0.00'  -- Use a money literal instead of an integer
-);
+WHERE ed IS NULL;
 
 SELECT SUM(capital_investment::numeric) / 1000000.0 AS fjtap_cap_invest_mil
 FROM ecd
-WHERE fjtap > '$0.00';
+WHERE fjtap IS NOT NULL;
+
 SELECT 
     county_tier, 
     AVG(new_jobs) AS avg_new_jobs
 FROM ecd
 GROUP BY county_tier;
+
 SELECT COUNT(DISTINCT company) AS llc_companies
 FROM ecd
 WHERE company ILIKE '%LLC%';
