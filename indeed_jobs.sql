c110f7b7c01a679c1483696ecc2aed73785a2082
lo0017 <lauren.odonnell99@gmail.com>
2026-01-30 15:13:47 -0500

    corrected

--- indeed_jobs.sql
+++ indeed_jobs.sql
@@ -38,51 +38,53 @@ GROUP BY location
 ORDER BY avg_rating DESC;
 
 ----Select unique job titles from the data_analyst_jobs table. How many are there? ---881
-SELECT DISTINCT title
+SELECT 
+	COUNT(DISTINCT title)
 FROM data_analyst_jobs;
 
 ----How many unique job titles are there for California companies?
-SELECT COUNT(DISTINCT title)
+SELECT 
+	COUNT(DISTINCT title)
 FROM data_analyst_jobs
 WHERE location = 'CA';
 
-----Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations? ----40
+----Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations? ----70
 SELECT 
     company, 
     AVG(star_rating) AS avg_rating
 FROM data_analyst_jobs
-WHERE review_count > 5000 
-  AND company IS NOT NULL
-GROUP BY company;
-
-----Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating? ----Unilever 4.2
-SELECT 
-    company, 
-    AVG(star_rating) AS avg_rating
-FROM data_analyst_jobs
-WHERE review_count > 5000 
-  AND company IS NOT NULL
+WHERE company IS NOT NULL
 GROUP BY company
+HAVING SUM (review_count) > 5000
+ORDER BY avg_rating;
+
+----Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating? ----Google 4.3
+SELECT 
+    company, 
+    AVG(star_rating) AS avg_rating
+FROM data_analyst_jobs
+WHERE company IS NOT NULL
+GROUP BY company
+HAVING SUM (review_count) > 5000
 ORDER BY avg_rating DESC;
 
 ----Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? ----1669
-SELECT title 
+SELECT
+	*
 FROM data_analyst_jobs 
-WHERE title ILIKE '%Analyst%';
+WHERE title ILIKE '%analyst%';
 
 ----How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common? ----Tableau
 SELECT COUNT(DISTINCT title)
 FROM data_analyst_jobs
-WHERE title NOT ILIKE '%Analyst%'
-  AND title NOT ILIKE '%Analytics%';
+WHERE title NOT ILIKE '%analyst%'
+  AND title NOT ILIKE '%analytics%';
 
 ----BONUS: You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks.Disregard any postings where the domain is NULL. Order your results so that the domain with the greatest number of hard to fill jobs is at the top. Which three industries are in the top 4 on this list? How many jobs have been listed for more than 3 weeks for each of the top 4?
 SELECT 
     domain, 
-    COUNT(*) AS job_count
+    COUNT(title) AS jobs
 FROM data_analyst_jobs
-WHERE skill ILIKE '%SQL%' 
-  AND days_since_posting > 21
-  AND domain IS NOT NULL
+WHERE domain IS NOT NULL AND skill ILIKE '%SQL%' AND days_since_posting > 21
 GROUP BY domain
-ORDER BY job_count DESC;
+ORDER BY jobs DESC;
